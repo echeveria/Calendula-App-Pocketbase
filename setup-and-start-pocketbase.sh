527556c09a882292e8bcbe2ef18a9003489d9af3
Stoyan Germanov <s.p.germanov@gmail.com>
2025-06-21 23:16:41 +0300

    use ai to fix dockerfile fix 4

--- setup-and-start-pocketbase.sh
+++ setup-and-start-pocketbase.sh
@@ -7,5 +7,4 @@ ADMIN_PASS=${ADMIN_PASS:-adminadmin}
 echo "admin used"
 $THIS_DIR/pocketbase superuser upsert ${ADMIN_USER} ${ADMIN_PASS}
 echo "try to run"
-
-exec pwd && $THIS_DIR/pocketbase serve --http 0.0.0.0:8090
+exec $THIS_DIR/pocketbase serve --http 0.0.0.0:8090
