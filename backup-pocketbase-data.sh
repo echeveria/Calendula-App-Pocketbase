a28becfeea3a262132575cc52791db862195f422
Stoyan Germanov <s.p.germanov@gmail.com>
2025-06-22 00:33:14 +0300

    new config

--- backup-pocketbase-data.sh
+++ backup-pocketbase-data.sh
@@ -1,28 +1,18 @@
 #!/bin/bash
+set -euo pipefail
 
 # This script backs up PocketBase data from a remote server to the local pb_data directory
 
 # Set default values for variables
-REMOTE_HOST=${REMOTE_HOST:-"angelator312.top"}
+REMOTE_HOST=${REMOTE_HOST:-"161.97.86.243"}
 REMOTE_USER=${REMOTE_USER:-"root"}
-REMOTE_PATH=${REMOTE_PATH:-"/pb_data/"}
-LOCAL_PATH=${LOCAL_PATH:-"$(dirname "$0")/pb_data"}
+REMOTE_PATH=${REMOTE_PATH:-"/var/lib/docker/volumes/2b9edbf182cd7838abf05668611d7b7b20ef06104a43a32dec9f2ae9791ded80/_data/"}
+LOCAL_PATH=${LOCAL_PATH:-"$(dirname "$0")/pb_data/"}
 
-# Display backup information
 echo "Backing up PocketBase data from ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_PATH} to ${LOCAL_PATH}"
-
-# Create local directory if it doesn't exist
 mkdir -p "${LOCAL_PATH}"
 
 # Perform the backup using rsync
-rsync -avz "${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_PATH}*" "${LOCAL_PATH}"
+rsync -avz "${LOCAL_PATH}" "${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_PATH}"
 
-# Check if the backup was successful
-if [ $? -eq 0 ]; then
-    echo "Backup completed successfully!"
-else
-    echo "Backup failed. Please check your connection and try again."
-    exit 1
-fi
-
-echo "Backup process completed."
+echo "✅ Backup completed successfully!"
