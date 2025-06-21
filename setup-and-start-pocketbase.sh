a1cc3571943044535ca3e87c3eb860f5f2e41def
Stoyan Germanov <s.p.germanov@gmail.com>
2025-06-21 22:36:11 +0300

    fix 3

--- setup-and-start-pocketbase.sh
+++ setup-and-start-pocketbase.sh
@@ -4,6 +4,7 @@ THIS_DIR=$(cd $(dirname $0); pwd)
 
 ADMIN_USER=${ADMIN_USER:-admin@example.com}
 ADMIN_PASS=${ADMIN_PASS:-adminadmin}
-
+echo "admin used"
 $THIS_DIR/pocketbase superuser upsert ${ADMIN_USER} ${ADMIN_PASS}
+echo "try to run"
 exec $THIS_DIR/pocketbase serve --http 0.0.0.0:8090
