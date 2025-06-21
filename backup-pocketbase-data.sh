b478b2c0b76c8dc578bcdfaf3deae35cbd505342
Stoyan Germanov <s.p.germanov@gmail.com>
2025-06-22 00:45:20 +0300

    revert

--- backup-pocketbase-data.sh
+++ backup-pocketbase-data.sh
@@ -6,7 +6,7 @@ set -euo pipefail
 # Set default values for variables
 REMOTE_HOST=${REMOTE_HOST:-"161.97.86.243"}
 REMOTE_USER=${REMOTE_USER:-"root"}
-REMOTE_PATH=${REMOTE_PATH:-"/var/lib/docker/volumes/2b9edbf182cd7838abf05668611d7b7b20ef06104a43a32dec9f2ae9791ded80/_data/"}
+REMOTE_PATH=${REMOTE_PATH:-"/var/lib/docker/volumes/e32164c738f071aa58e5f8cf29aa65c66ce358472d27ff06b690db24bc77b07d/_data/"}
 LOCAL_PATH=${LOCAL_PATH:-"$(dirname "$0")/pb_data/"}
 
 echo "Backing up PocketBase data from ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_PATH} to ${LOCAL_PATH}"
--- setup-and-start-pocketbase.sh
+++ setup-and-start-pocketbase.sh
@@ -7,4 +7,4 @@ ADMIN_PASS=${ADMIN_PASS:-adminadmin}
 echo "admin used"
 $THIS_DIR/pocketbase superuser upsert ${ADMIN_USER} ${ADMIN_PASS}
 echo "try to run"
-exec $THIS_DIR/pocketbase serve --http 0.0.0.0:8090 --dir /pb_data
+exec $THIS_DIR/pocketbase serve --http 0.0.0.0:8090
