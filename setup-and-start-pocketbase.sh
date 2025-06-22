eca495079babc0e4c75086e5ec7c19f246ca8590
Stoyan Germanov <s.p.germanov@gmail.com>
2025-06-22 15:56:29 +0300

    text tag 1

--- setup-and-start-pocketbase.sh
+++ setup-and-start-pocketbase.sh
@@ -4,8 +4,6 @@ THIS_DIR=$(cd $(dirname $0); pwd)
 
 ADMIN_USER=${ADMIN_USER:-admin@example.com}
 ADMIN_PASS=${ADMIN_PASS:-adminadmin}
-echo "admin used"
-$THIS_DIR/pocketbase superuser upsert ${ADMIN_USER} ${ADMIN_PASS}
-echo "try to run"
 
-exec  $THIS_DIR/pocketbase serve --http 0.0.0.0:8090
+$THIS_DIR/pocketbase superuser upsert ${ADMIN_USER} ${ADMIN_PASS}
+exec $THIS_DIR/pocketbase serve --http 0.0.0.0:8090
